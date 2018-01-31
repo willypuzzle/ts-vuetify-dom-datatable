5a74196ee4365e0f03fffb7d5429b140f90f4428
Domenico Rizzo <dev@idsign.it>
2018-01-31 18:00:00 +0100

    Added mixin

---
+++ mixins/JsonFieldSupport.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import Vue from 'vue';
+    import _ from 'lodash';
+    import axios from 'axios'
+    import randomstring from 'randomstring'
+
+    export default Vue.extend({
+        methods: {
+            changeJson(value, item, jsonPath, field = 'label', urlBuilderFunction : Function = null){
+                let a = this.$axios || axios;
+                let updateFunctionUrl = _.get(this, 'transport.update.url', null);
+                let updateUrl : any = null;
+                if(updateFunctionUrl){
+                    updateUrl = updateFunctionUrl(item)
+                }else if(urlBuilderFunction){
+                    updateUrl = urlBuilderFunction(item)
+                }else{
+                    throw `changeLabel: url builder function not found`
+                }
+                a.put(updateUrl, {
+                    field: field,
+                    [field]: value,
+                    path: jsonPath
+                })
+            },
+            manageJsonInCreation(value, models, pathField, randomField = null, field = 'label', path = 'locale'){
+                if(randomField){
+                    models[randomField] = randomstring.generate({
+                        length: 32,
+                        charset: 'hex'
+                    });
+                }
+
+                models[field] = value;
+                models[path] = pathField;
+            }
+        }
+    });
+</script>
